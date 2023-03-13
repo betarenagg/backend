705a2dcfff9045a7992fd227a64750b3182b0f32
Valiant Joe <highscoretechng@gmail.com>
2023-03-13 21:14:20 +0100

    all api

--- App.js
+++ App.js
@@ -3,32 +3,13 @@ const mongoose = require('mongoose')
 const Users = require('./routes/Users')
 const cors = require('cors');
 
-var bodyParser = require('body-parser')
-
 
 require('dotenv').config()
 const app = express()
 
 
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 app.use(express.json())
 app.use(cors())
-
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-      console.log('User Disconnected');
-    });
-  socket.on('example_message', function(msg){
-      console.log('message: ' + msg);
-    });
-  });
-  io.listen(8500)
 
 app.use('/api/users', Users)
 
