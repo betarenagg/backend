89f7f33eacdede194b508957687ab670e1bdcf17
Valiant Joe <highscoretechng@gmail.com>
2023-03-16 11:28:23 +0100

    oaky

--- controller/Transaction.js
+++ controller/Transaction.js
@@ -1,27 +1,26 @@
-// const deposit = require('../model/deposit')
+const deposit = require('../model/deposit')
 
+//..create a new deposit 
+const Deposit = async (req, res) => {
 
-// //..create a new deposit 
-// const Deposit = async (req, res) => {
+    const { coin_name,  coin_amount, gas_fee, usd_amount, wallet_address,  network_address } = req.body
 
-//     const { coin_name,  coin_amount, gas_fee, usd_amount, wallet_address,  network_address } = req.body
+    if( !coin_name || !coin_amount || !gas_fee || !wallet_address || !usd_amount || !network_address){
+        res.status(401).json({error : "All field is required"})
+    }else{
+        const user_id = req.user._id
+        const status = "pending"
+        const  date_time = new Date()
+        const type = `Deposit`
+    try{
+    const depositData = await deposit.create({user_id, coin_name,  coin_amount, usd_amount, wallet_address,gas_fee,  network_address,type, status, date_time })
 
-//     if( !coin_name || !coin_amount || !gas_fee || !wallet_address || !usd_amount || !network_address){
-//         res.status(401).json({error : "All field is required"})
-//     }else{
-//         const user_id = req.user._id
-//         const status = "pending"
-//         const  date_time = new Date()
-//         const type = `Deposit`
-//     try{
-//     const depositData = await deposit.create({user_id, coin_name,  coin_amount, usd_amount, wallet_address,gas_fee,  network_address,type, status, date_time })
-
-//        res.status(200).json(depositData)
-//     } catch(error){
-//         res.status(500).json({message: err.message})
-//     }
-// }
-// }
+       res.status(200).json(depositData)
+    } catch(error){
+        res.status(500).json({message: err.message})
+    }
+}
+}
 
 // //..create a new deposit 
 // const Withdraw = async (req, res) => {
